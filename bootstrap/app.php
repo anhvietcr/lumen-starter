7a54acda56ee2fa93f490730551f3e5a061bc087
Ben Kuhl <benkuhl@gmail.com>
2016-09-08 08:45:39 -0400

    Upgraded to Lumen 5.3 (#90)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
+    (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
@@ -20,7 +20,7 @@ try {
 */
 
 $app = new Laravel\Lumen\Application(
-    realpath(__DIR__ . '/../')
+    realpath(__DIR__.'/../')
 );
 
 $app->withFacades();
@@ -80,7 +80,7 @@ $app->routeMiddleware(['jsonApi.enforceMediaType' => RealPage\JsonApi\Lumen\Enfo
 |
 */
 
- $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(RealPage\JsonApi\Lumen\ServiceProvider::class);
@@ -102,7 +102,7 @@ if (class_exists('Vluzrmos\Tinker\TinkerServiceProvider')) {
 */
 
 $app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
-    require __DIR__ . '/../app/Http/routes.php';
+    require __DIR__.'/../app/Http/routes.php';
 });
 
 return $app;
